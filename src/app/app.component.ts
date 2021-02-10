8615a30f7c305604b4e409343d1adde940953a34
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-02-10 11:02:16 +0200

    Add Async pipe to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-
-  courses;;
-
+  courses$ : Observable<Course[]>; 
+  
   constructor(private http:HttpClient) {
 
   }
@@ -26,10 +25,8 @@ export class AppComponent implements OnInit {
       .set("page", "1")
       .set("pageSize", "10");
 
-    this.http.get('/api/courses', {params})
-      .subscribe(
-        courses => this.courses = courses
-      );
+    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
+      
   }
 
 
